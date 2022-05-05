0c51b34de9827d2a76dc492b3398e5fd5fb37ab1
hidetoshi <phunganhtuan.nd.vn@gmail.com>
2022-05-05 12:38:50 +0900

    update paiza C039

--- paizaC031.py
+++ paizaC031.py
@@ -1,9 +1,12 @@
+from unittest import TestCase
+
+
+# 0/10 TestCase
 N = int(input())
 time_diff_list = []
 hour_output= []
-minute_output = []
 city_output= []
-GMT_time = 0
+time_output = 0
 
 for i in range(0,N):
     i_n = input().split(' ')
@@ -14,21 +17,27 @@ city_and_time_inp = input()
 city_inp_list = city_and_time_inp.split(' ')[0]
 time_inp_list = city_and_time_inp.split(' ')[-1].split(':')
 
+if int(time_inp_list[0]) == 0:
+    time_inp_list[0] = 24
+    
+
 for j in range(len(city_output)):
     if city_output[j] == city_inp_list:
-        GMT_time = int(time_inp_list[0]) - time_diff_list[j]
-        break
- 
-for k in range(len(city_output)):
-    if GMT_time >= 24:
-        GMT_time_new = GMT_time -24
-        if time_diff_list[k] < 0:
-            hour_output.append(GMT_time + time_diff_list[k])
+        if int(time_inp_list[0]) >= int(time_diff_list[j]):
+            GMT_time = int(time_inp_list[0]) - time_diff_list[j]
         else:
-            hour_output.append(GMT_time_new + time_diff_list[k])
-    else:
-        hour_output.append(GMT_time + time_diff_list[k])
-        
-    print('{:02d}:{:02d}'.format(int(hour_output[k]),int(time_inp_list[-1])))
+            GMT_time = int(time_inp_list[0]) + time_diff_list[j]
+        break
 
+for k in range(len(city_output)):
     
+    time_output = GMT_time + time_diff_list[k]
+    if time_output < 24:
+        hour_output.append(time_output)
+    else:
+        while True:
+            time_output = time_output - 24
+            if(time_output >= 0 and time_output < 24):
+                break
+        hour_output.append(time_output)
+    print('{:02d}:{:02d}'.format(int(hour_output[k]),int(time_inp_list[-1])))
